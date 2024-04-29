ddc1345bf8c068eb40b3dae10fddbd040335760a
Berk Selvi <Berk.Selvi@d-teknoloji.com.tr>
2024-04-29 13:27:17 +0300

    Inital commit

---
+++ ocelot/Ocelot.Gateway/Program.cs
@@ -0,0 +1,10 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+
+builder.Services.AddOcelot(builder.Configuration);
+
+var app = builder.Build();
+
+await app.UseOcelot();
+await app.RunAsync();
---
+++ service/Service.API/Program.cs
@@ -0,0 +1,26 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MapGet("/weather", () =>
+{
+    Task.Delay(1000).Wait();
+
+    return "Weather is OK!";
+})
+.WithName("GetWeather")
+.WithOpenApi();
+
+app.Run();
